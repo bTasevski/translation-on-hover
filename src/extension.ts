3b37a282eaff71de1b2282bb71bdda972b21ef09
Bojan Tasevski <bojan.tasevski@guestline.com>
2023-11-13 17:28:39 +0100

    refactor

--- src/extension.ts
+++ src/extension.ts
@@ -2,63 +2,61 @@ import * as vscode from "vscode";
 import * as fs from "fs";
 import * as path from "path";
 
+type TranslationKeyWithFileName = {
+  jsonFileName: string;
+  translationsKey: string;
+};
+
+const TRANSLATIONS_FOLDERS_PATHS = ["public/translations", "translations"];
+
+const LOCALE = "en-GB";
+
+// matches patterns:
+// 1) t("message")
+// 2) t("message"
+// 3) i18nKey="message"
+const T_REGEX =
+  /t\(["']([^"']+)["']\s*[^)]*|i18nKey=["']([^"':]+):([^"']+)["']/;
+
 export function activate(context: vscode.ExtensionContext) {
   console.log(
     'Congratulations, your extension "hover-translation" is now active!'
   );
   let disposable = vscode.languages.registerHoverProvider("*", {
     provideHover(document, position) {
-      // matches patterns:
-      // 1) t("message")
-      // 2) t("message"
-      // 3) i18nKey="message"
-      const tRegex =
-        /t\(["']([^"']+)["']\s*[^)]*|i18nKey=["']([^"':]+):([^"']+)["']/;
-      const range = document.getWordRangeAtPosition(position, tRegex);
-      if (range) {
-        const text = document.getText(range);
-        const match = tRegex.exec(text);
-        if (match) {
-          let argument = match[1];
-          if (match[0].toString().includes("i18n")) {
-            argument = `${match[2]}:${match[3]}`;
-          }
-          if (argument) {
-            const [jsonFileName, translationsKey] = argument.split(":");
-
-            const translationFolders = ["public/translations", "translations"];
-
-            const currentFilePath = document.uri.fsPath;
-            const nearestTranslationFolder = findNearestTranslationFolder(
-              currentFilePath,
-              translationFolders,
-              "en-GB"
-            );
-
-            if (nearestTranslationFolder) {
-              const jsonFilePath = findJsonFile(
-                nearestTranslationFolder,
-                jsonFileName
-              );
-
-              if (jsonFilePath) {
-                const jsonContent = fs.readFileSync(jsonFilePath, "utf-8");
-                const translations = JSON.parse(jsonContent);
-                const translationValue = translations[translationsKey];
-
-                if (translationValue) {
-                  const hoverMessage = new vscode.MarkdownString(
-                    `🌐${translationValue}🌐`
-                  );
-
-                  return new vscode.Hover(hoverMessage, range);
-                }
-              }
-            }
-          }
-        }
+      const matchedText = matchTranslation(document, position);
+      if (!matchedText) {
+        return undefined;
       }
-      return undefined;
+
+      const { translationsKey, jsonFileName } =
+        getJsonFileNameAndTranslationKey(matchedText);
+
+      const currentFilePath = document.uri.fsPath;
+      const nearestTranslationFolder = findNearestTranslationFolder(
+        currentFilePath,
+        TRANSLATIONS_FOLDERS_PATHS,
+        LOCALE
+      );
+
+      if (!Boolean(nearestTranslationFolder)) {
+        return undefined;
+      }
+
+      const translationsFilePath = findJsonFile(
+        nearestTranslationFolder!,
+        jsonFileName
+      );
+
+      if (!Boolean(translationsFilePath)) {
+        return undefined;
+      }
+      console.log("qw23er123123");
+      const jsonContent = fs.readFileSync(translationsFilePath!, "utf-8");
+      const translations = JSON.parse(jsonContent);
+      const translatedValue = translations[translationsKey] as string;
+
+      return createHoverMessage(translatedValue, document, position);
     },
   });
 
@@ -119,5 +117,50 @@ const isWithinWorkspace = (
     dir.startsWith(workspaceFolder.uri.fsPath)
   );
 };
+
+function getJsonFileNameAndTranslationKey(
+  matchedText: RegExpExecArray
+): TranslationKeyWithFileName {
+  let argument = matchedText[1];
+  if (matchedText[0].toString().includes("i18n")) {
+    argument = `${matchedText[2]}:${matchedText[3]}`;
+  }
+  const [jsonFileName, translationsKey] = argument.split(":");
+  return { translationsKey, jsonFileName } as TranslationKeyWithFileName;
+}
+
+function matchTranslation(
+  document: vscode.TextDocument,
+  position: vscode.Position
+): RegExpExecArray | undefined {
+  const range = getRange(document, position);
+  if (range) {
+    const text = document.getText(range);
+    const match = T_REGEX.exec(text);
+    if (match) {
+      return match;
+    }
+    return undefined;
+  }
+}
+
+function createHoverMessage(
+  translatedText: string,
+  document: vscode.TextDocument,
+  position: vscode.Position
+): vscode.Hover {
+  const range = getRange(document, position);
+
+  const hoverMessage = new vscode.MarkdownString(`🌐 ${translatedText}`);
+
+  return new vscode.Hover(hoverMessage, range);
+}
+
+function getRange(
+  document: vscode.TextDocument,
+  position: vscode.Position
+): vscode.Range | undefined {
+  return document.getWordRangeAtPosition(position, T_REGEX);
+}
 
 export function deactivate() {}
